a24cca1a5d000533259b833a664beaf57d7bc359
creepystefan <147045262+creepystefan@users.noreply.github.com>
2025-06-17 07:28:52 +0200

    Update __init__.py

--- components/dimplex/sensor/__init__.py
+++ components/dimplex/sensor/__init__.py
@@ -1,12 +1,12 @@
 import esphome.codegen as cg
 from esphome.components import sensor
 import esphome.config_validation as cv
-from esphome.const import (
+#from esphome.const import (
     
-    CONF_ID,
-    UNIT_EMPTY,
-    ICON_EMPTY
-)
+ #   CONF_ID,
+  #  UNIT_EMPTY,
+  #  ICON_EMPTY
+#)
 
 CODEOWNERS = ["@creepystefan"]
 
@@ -27,8 +27,9 @@ CONFIG_SCHEMA = cv.Schema(
         {
         cv.GenerateID(): cv.declare_id(DimplexTCP),
         cv.Required(CONF_IP_ADDRESS): cv.ipv4address,
-      #  cv.Optional(CONF_PORT, default=502): cv.int_range(min=0, max=10000),
+        cv.Optional(CONF_PORT, default=502): cv.int_range(0, 65535),
         cv.Optional(CONF_FUNCTIONCODE, default=4): cv.int_range(min=0, max=10),
+        cv.Required(CONF_FUNCTIONCODE): cv.int_range(min=0, max=4),
         cv.Required(CONF_REGISTER_ADDRESS): cv.int_,
      #   cv.Optional(CONF_UPDATE_INTERVAL, default=1000): cv.int_range(min=500, max=10000),
         }
@@ -44,4 +45,5 @@ async def to_code(config):
     cg.add(var.set_host(str(config[CONF_IP_ADDRESS])))
     cg.add(var.set_functioncode(config[CONF_FUNCTIONCODE]))
     cg.add(var.set_register_address(config[CONF_REGISTER_ADDRESS]))
+    cg.add(var.set_port(config["port"]))
    # cg.add(var.update_interval(config[CONF_UPDATE_INTERVAL]))
