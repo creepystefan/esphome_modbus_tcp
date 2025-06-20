f042b73eb962978490b5ce1127566743402448a5
creepystefan <147045262+creepystefan@users.noreply.github.com>
2025-06-20 07:54:03 +0200

    Update dimplex.h

--- components/dimplex/sensor/dimplex.h
+++ components/dimplex/sensor/dimplex.h
@@ -32,24 +32,24 @@ WiFiClient client;
       return;
     }
 
-    // Build Modbus request (Read Holding Registers, function code 0x03)
-    uint8_t request[] = {
+
+  uint8_t request[] = {
         0x00, 0x08,  // Transaction ID
         0x00, 0x00,  // Protocol ID
         0x00, 0x06,  // Length
         0x01,        // Unit ID
         functioncode_,
-        //0x04,        // Function Code (COIL)
         (uint8_t)((register_address_ >> 8) & 0xFF),  // Start Address (High Byte)
         (uint8_t)(register_address_ & 0xFF),        // Start Address (Low Byte)
-        0x00, 0x02   // Quantity (Read 2 Registers = 32 bits for FP32)
+        0x00, 0x01   // Quantity (Read 2 Registers = 32 bits for FP32)
         };
-    ESP_LOGD("TX", "Address: %d %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X",
+    ESP_LOGD("TX", "Address: %d >>>> %02X%02X %02X%02X %02X%02X %02X %02X %02X%02X %02X%02X",
                         this->register_address_,
                         request[0], request[1], request[2], request[3], request[4]
                         , request[5], request[6], request[7], request[8]
                         , request[9], request[10], request[11]
                         );
+
 client.write(request, sizeof(request));
 delay(100);
 
@@ -67,29 +67,17 @@ if (response[1] != request[1]) {
     }
 
  
-    ESP_LOGD("RX", "Address: %d %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X",
+    ESP_LOGD("RX", "Address: %d <<<< %02X%02X %02X%02X %02X%02X %02X %02X %02X %02X%02X ",
                       this->register_address_,
                       response[0], response[1], response[2], response[3], response[4], 
                       response[5], response[6], response[7], response[8], response[9], 
-                      response[10], response[11], response[12]
-                      );
-float value = decode_float(&response[9]);
-ESP_LOGD("modbus_tcp", "Register %d: %.2f", register_address_, value);
+                      response[10]                      );
+
+unsigned int value = (response[9] << 8) | response[10];
 publish_state(value);
 
-
 }
-
- float decode_float(uint8_t *data) {
-    uint32_t raw_value = 0;
-    raw_value = (data[0] << 24) | (data[1] << 16) | (data[2] << 8) | data[3];
-    float value;
-    memcpy(&value, &raw_value, sizeof(float));
-    return value;
-  }
-
-
-
+ 
 void dump_config() override;
 
 protected:
