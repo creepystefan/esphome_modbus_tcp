ca7fb3d73b24b8b5a7c09d4d3772c5090bfc6462
creepystefan <147045262+creepystefan@users.noreply.github.com>
2025-06-20 08:33:02 +0200

    Update dimplex.cpp

--- components/dimplex/sensor/dimplex.cpp
+++ components/dimplex/sensor/dimplex.cpp
@@ -7,6 +7,76 @@ namespace dimplex {
 static const char *const TAG = "Dimplex-Sensor-Modbus";
 
 
+
+void DimplexTCP::update() {
+WiFiClient client;
+    if (!client.connect(host_.c_str(), port_)) {
+      ESP_LOGE("modbus_tcp", "Failed to connect to Modbus server %s:%d", host_.c_str(), port_);
+      return;
+    }
+
+    // Build Modbus request (Read Holding Registers, function code 0x03)
+  
+//uint8_t testresponse[] = {
+//        0x00, 0x08, 0x00, 0x00, 0x01, 0x06, 0x01, 0x04,  0x00, 0x02
+//        };
+
+
+  uint8_t request[] = {
+        0x00, 0x08,  // Transaction ID
+        0x00, 0x00,  // Protocol ID
+        0x00, 0x06,  // Length
+        0x01,        // Unit ID
+        functioncode_,
+        //0x04,        // Function Code (COIL)
+        (uint8_t)((register_address_ >> 8) & 0xFF),  // Start Address (High Byte)
+        (uint8_t)(register_address_ & 0xFF),        // Start Address (Low Byte)
+        0x00, 0x01   // Quantity (Read 2 Registers = 32 bits for FP32)
+        };
+    ESP_LOGD("TX", "Address: %d >>>> %02X%02X %02X%02X %02X%02X %02X %02X %02X%02X %02X%02X",
+                        this->register_address_,
+                        request[0], request[1], request[2], request[3], request[4]
+                        , request[5], request[6], request[7], request[8]
+                        , request[9], request[10], request[11]
+                        );
+
+client.write(request, sizeof(request));
+delay(100);
+
+uint8_t response[256];
+    size_t response_len = client.read(response, sizeof(response));
+
+if (response[7] != request[7]) {
+      ESP_LOGE("modbus_tcp", "Unexpected function code: 0x%02X", response[7]);
+      return;
+    }
+
+if (response[1] != request[1]) {
+      ESP_LOGE("modbus_tcp", "Unexpected Transaction ID: 0x%02X", response[1]);
+      return;
+    }
+
+ 
+    ESP_LOGD("RX", "Address: %d <<<< %02X%02X %02X%02X %02X%02X %02X %02X %02X %02X%02X ",
+                      this->register_address_,
+                      response[0], response[1], response[2], response[3], response[4], 
+                      response[5], response[6], response[7], response[8], response[9], 
+                      response[10]                      );
+//float value = decode_float(&response[9]);
+//ESP_LOGD("modbus_tcp", "Register %d: %.2f", register_address_, value);
+
+unsigned int value = (response[9] << 8) | response[10];
+//ESP_LOGD("RX", "CombinedValue: %.2d", value);
+
+publish_state(value);
+
+}
+
+
+
+
+
+
 void DimplexTCP::dump_config() {
 //  ESP_LOGCONFIG(TAG, "Sensor");
   ESP_LOGCONFIG(TAG, "Address: %d", this->register_address_);
