e0a2b08dafd68cb62839802c701ff37355c51af7
creepystefan <147045262+creepystefan@users.noreply.github.com>
2025-09-17 10:49:54 +0200

    Update modbus_tcp_32.h

--- components/modbus_tcp_32/sensor/modbus_tcp_32.h
+++ components/modbus_tcp_32/sensor/modbus_tcp_32.h
@@ -15,25 +15,17 @@ class ModbusTCP32 :  public sensor::Sensor, public PollingComponent {
 
 float get_setup_priority() const override { return esphome::setup_priority::AFTER_WIFI; }
 
-//void update_interval(uint32_t update_interval) { this->update_interval_ = update_interval; }
 void set_host(const std::string &host) { this->host_ = host; }
 void set_port(uint16_t port) { this->port_ = port; }
 void set_functioncode(uint8_t functioncode) { this->functioncode_ = functioncode; }
 void set_register_address(uint16_t register_address) { this->register_address_ = register_address; }
+void set_unit_id(uint8_t unit_id) { this->unit_id_ = unit_id; }
 
 
 void setup() override{
 }
 
 void update() override;
-
-// float decode_float(uint8_t *data) {
-//    uint32_t raw_value = 0;
-//    raw_value = (data[0] << 24) | (data[1] << 16) | (data[2] << 8) | data[3];
-//    float value;
-//    memcpy(&value, &raw_value, sizeof(float));
-//    return value;
-//  }
 
 
 
@@ -42,7 +34,7 @@ void dump_config() override;
 protected:
   uint16_t register_address_;
   uint8_t functioncode_;
-//  uint32_t update_interval_;
+  uint8_t unit_id_;
   uint16_t port_;
   std::string host_;
 
