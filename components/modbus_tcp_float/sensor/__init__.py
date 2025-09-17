73a079d4d6af190469c34a26364dd8fc33ad7b59
creepystefan <147045262+creepystefan@users.noreply.github.com>
2025-09-17 10:21:07 +0200

    Update __init__.py

--- components/modbus_tcp_float/sensor/__init__.py
+++ components/modbus_tcp_float/sensor/__init__.py
@@ -9,6 +9,7 @@ CONF_REGISTER_ADDRESS =  'register_address'
 #CONF_UPDATE_INTERVAL =  'update_interval'
 CONF_IP_ADDRESS = 'host'
 CONF_PORT = 'port'
+CONF_UNIT_ID = 'unit_id'
 
 
 modbus_tcp_float_ns = cg.esphome_ns.namespace("modbus_tcp_float")
@@ -22,8 +23,8 @@ CONFIG_SCHEMA = cv.Schema(
         cv.GenerateID(): cv.declare_id(ModbusTCPfloat),
         cv.Required(CONF_IP_ADDRESS): cv.ipv4address,
         cv.Optional(CONF_PORT, default=502): cv.int_range(0, 65535),
-        cv.Optional(CONF_FUNCTIONCODE, default=4): cv.int_range(min=0, max=10),
-        cv.Required(CONF_FUNCTIONCODE): cv.int_range(min=0, max=4),
+        cv.Optional(CONF_FUNCTIONCODE, default=4): cv.int_range(min=1, max=4),
+        cv.Optional(CONF_UNIT_ID, default=1): cv.int_range(min=0, max=255),
         cv.Required(CONF_REGISTER_ADDRESS): cv.int_,
      #   cv.Optional(CONF_UPDATE_INTERVAL, default=1000): cv.int_range(min=500, max=10000),
         }
@@ -38,6 +39,7 @@ async def to_code(config):
     await cg.register_component(var, config)
     cg.add(var.set_host(str(config[CONF_IP_ADDRESS])))
     cg.add(var.set_functioncode(config[CONF_FUNCTIONCODE]))
+    cg.add(var.set_unit_id(config[CONF_UNIT_ID]))
     cg.add(var.set_register_address(config[CONF_REGISTER_ADDRESS]))
     cg.add(var.set_port(config["port"]))
    # cg.add(var.update_interval(config[CONF_UPDATE_INTERVAL]))
