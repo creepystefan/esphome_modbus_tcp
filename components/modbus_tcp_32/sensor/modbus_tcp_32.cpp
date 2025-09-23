9d18f0ac0372164a2f3e82fa2cf978fa515b91e5
creepystefan <147045262+creepystefan@users.noreply.github.com>
2025-09-23 08:13:29 +0200

    Update modbus_tcp_32.cpp

--- components/modbus_tcp_32/sensor/modbus_tcp_32.cpp
+++ components/modbus_tcp_32/sensor/modbus_tcp_32.cpp
@@ -23,21 +23,27 @@ WiFiClient client;
         functioncode_,
         (uint8_t)((register_address_ >> 8) & 0xFF),  // Start Address (High Byte)
         (uint8_t)(register_address_ & 0xFF),        // Start Address (Low Byte)
-        0x00, 0x01   // Quantity (Read 2 Registers = 32 bits for FP32)
+        0x00, 0x02   // Quantity (Read 2 Registers = 32 bits for FP32)
         };
     ESP_LOGD("TX", "Address: %d >>>> %02X%02X %02X%02X %02X%02X %02X %02X %02X%02X %02X%02X",
                         this->register_address_,
                         request[0], request[1], request[2], request[3], request[4]
                         , request[5], request[6], request[7], request[8]
-                        , request[9], request[10], request[11] 
+                        , request[9], request[10], request[11]
                         );
 
 client.write(request, sizeof(request));
-delay(100);
+delay(timeout_);
 
 uint8_t response[256];
     size_t response_len = client.read(response, sizeof(response));
-
+ 
+    ESP_LOGD("RX", "Address: %d <<<< %02X%02X %02X%02X %02X%02X %02X %02X %02X %02X%02X%02X%02X ",
+                      this->register_address_,
+                      response[0], response[1], response[2], response[3], response[4], 
+                      response[5], response[6], response[7], response[8], response[9], 
+                      response[10], response[11], response[12]     );
+    
 if (response[7] != request[7]) {
       ESP_LOGE("modbus_tcp", "Unexpected function code: 0x%02X", response[7]);
       return;
@@ -47,13 +53,6 @@ if (response[1] != request[1]) {
       ESP_LOGE("modbus_tcp", "Unexpected Transaction ID: 0x%02X", response[1]);
       return;
     }
-
- 
-    ESP_LOGD("RX", "Address: %d <<<< %02X%02X %02X%02X %02X%02X %02X %02X %02X %02X%02X%02X%02X ",
-                      this->register_address_,
-                      response[0], response[1], response[2], response[3], response[4], 
-                      response[5], response[6], response[7], response[8], response[9], 
-                      response[10], response[11], response[12]     );
 
 unsigned int value = (response[9] << 24) | (response[10] << 16) | (response[11] << 8) | response[12];
 publish_state(value);
