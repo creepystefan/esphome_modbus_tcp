febdbf18ed1219756f54c72a3e211551a91cfc30
creepystefan <147045262+creepystefan@users.noreply.github.com>
2026-06-15 08:47:06 +0200

    Update modbustcp.cpp

--- components/modbustcp/modbustcp.cpp
+++ components/modbustcp/modbustcp.cpp
@@ -134,9 +134,6 @@ void ModbusTCP::setup() {
     }
    }
  
-
-
-
   void ModbusTCP::on_shutdown() {
     if (client_) {
       client_->close();
@@ -186,11 +183,7 @@ void ModbusTCP::send(uint8_t address, uint8_t function_code, uint16_t start_addr
     ESP_LOGE(TAG, "send too many values %d max=%zu", number_of_entities, MAX_VALUES);
     return;
   }
-  // if (!client.connect(host_.c_str(), port_)) {
-  //    ESP_LOGE("modbus_tcp", "Failed to connect to Modbus server %s:%d", host_.c_str(), port_);
-  //    return;
-  //  }
-  
+    
   size_t MAX_FRAME_SIZE = 256;
   uint8_t data[MAX_FRAME_SIZE];
   size_t pos = 0;
@@ -200,8 +193,7 @@ void ModbusTCP::send(uint8_t address, uint8_t function_code, uint16_t start_addr
       data[pos++] = 0x00;
       data[pos++] = 0x00;
         if (payload != nullptr) { 
-         //  data_send.push_back(0x04 + payload_len);
-        data[pos++] = (0x04 + payload_len);
+          data[pos++] = (0x04 + payload_len);
         }else {
         data[pos++] = 0x06;      // how many bytes next comes for request
       }
@@ -216,59 +208,7 @@ void ModbusTCP::send(uint8_t address, uint8_t function_code, uint16_t start_addr
       }
       Transaction_Identifier++;
  send_message(data);
-  /*
- std::vector<uint8_t> data_send;
  
-      data_send.push_back(Transaction_Identifier >> 8);
-      data_send.push_back(Transaction_Identifier >> 0);
-      data_send.push_back(0x00);
-      data_send.push_back(0x00);
-      data_send.push_back(0x00);
-      if (payload != nullptr) { 
-        data_send.push_back(0x04 + payload_len);
-      }else {
-        data_send.push_back(0x06);      // how many bytes next comes
-      }
-     data_send.push_back(address);
-     data_send.push_back(function_code);
-     data_send.push_back(start_address >> 8);
-     data_send.push_back(start_address >> 0);
-    // function nicht 5 oder nicht 6
-     if (function_code != 0x05 && function_code != 0x06) {
-      data_send.push_back(number_of_entities >> 8);
-      data_send.push_back(number_of_entities >> 0);
-     }
-  
-  if (payload != nullptr) {
-    if (function_code == 0x0F || function_code == 0x17) {  // Write multiple
-      data_send.push_back(payload_len);                    // Byte count is required for write
-    } else {
-      payload_len = 2;  // Write single register or coil
-    }
-    for (int i = 0; i < payload_len; i++) {
-      data_send.push_back(payload[i]);
-    }
-  }
- 
-
- std::string res1;
- char buf1[5];
- size_t len1 = 11; 
- for (size_t i = 12; i < data_send[5] + 6; i++) {
- sprintf(buf1, "%02X", data_send[i]);
-res1 += buf1;
-res1 += ":";
-}
-   if (connected_ && client_->canSend()) {
-    //if (connected_) {
-    //client_->write(reinterpret_cast<const char*>(data_send.data()), sizeof(data_send));
-    Transaction_Identifier++;
-    //client.write(reinterpret_cast<const char*>(data_send.data()), sizeof(data_send));
-    ESP_LOGD(TAG, ">>> %02X%02X %02X%02X %02X%02X %02X %02X %02X%02X %02X%02X %s",
-                   data_send[0], data_send[1],  data_send[2], data_send[3], data_send[4], data_send[5],
-                   data_send[6], data_send[7],  data_send[8], data_send[9], data_send[10], data_send[11], res1.c_str());
-   }
-*/
 waiting_for_response = address;
 last_send_ = millis();
 }
