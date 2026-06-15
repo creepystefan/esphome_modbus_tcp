b3d4e0e7a606ded84812632bcebd64c5f41630b1
creepystefan <147045262+creepystefan@users.noreply.github.com>
2026-06-15 08:45:02 +0200

    Update modbustcp.cpp

--- components/modbustcp/modbustcp.cpp
+++ components/modbustcp/modbustcp.cpp
@@ -46,8 +46,43 @@ void ModbusTCP::setup() {
     }
   }
 
-  void ModbusTCP::handle_message(uint8_t byte[256]) {
+  void ModbusTCP::send_message(const uint8_t *send_byte) {
+    if (connected_ && client_->canSend()) {
+      //client_->write(message.c_str());
+      //client_->write(reinterpret_cast<const char*>(frame.data.get()), sizeof(frame.data));
+
+    
+   //for (size_t i = 0; i < sizeof(send_byte); i++) {
+    //sprintf(buf1, "%02X", send_byte[i]);
+
+      client_->write(reinterpret_cast<const char*>(send_byte), (send_byte[5] + 6));
       
+        
+     std::string res1;
+     char buf1[5];
+      
+     //for (size_t i = 12; i < send_byte[5] + 6; i++) {
+     for (size_t i = 12; i < send_byte[5] + 6; i++) {
+     sprintf(buf1, "%02X", send_byte[i]);
+     res1 += buf1;
+     res1 += ":";
+     }
+     ESP_LOGD("modbus_tcp", ">>> %02X%02X %02X%02X %02X%02X %02X %02X %02X%02X %02X%02X %s",
+                   send_byte[0], send_byte[1],  send_byte[2], send_byte[3], send_byte[4], send_byte[5],
+                   send_byte[6], send_byte[7],  send_byte[8], send_byte[9], send_byte[10], send_byte[11], res1.c_str());
+                  
+      } else {
+      ESP_LOGW("modbus_tcp", "Cannot send, not connected");
+    }
+  }
+
+  void ModbusTCP::handle_message(uint8_t byte[256]) {
+    //const uint32_t now = App.get_loop_component_start_time();
+    //const uint32_t now = App.get_loop_component_start_time();
+
+    //uint8_t *bytedata = (uint8_t*)&byte;
+   //size_t len = sizeof(rx_data_);
+  
   uint8_t bytelen_len = 9;
   size_t data_len = byte[8];
   uint8_t address = byte[6];
@@ -71,8 +106,8 @@ void ModbusTCP::setup() {
                       byte[5], byte[6], byte[7], byte[8], res.c_str());
    
    
-   if ((byte[7] & 0x80) == 0x80) {
-      ESP_LOGE(TAG,"Error:"); 
+   if ((byte[7] & 0x80) == 0x80 || (byte[7] & 0x81) == 0x81) {
+      ESP_LOGE(TAG,"Error: "); 
     if (byte[8]  == 0x01) {
       ESP_LOGE(TAG,"Failure Code 0x01 ILLEGAL FUNCTION");
     }
@@ -93,6 +128,7 @@ void ModbusTCP::setup() {
     }
     return;
   }
+  
    for (auto *device : this->devices_) {
           device->on_modbus_data(data);
     }
@@ -139,7 +175,7 @@ void ModbusTCP::dump_config() {
                          host_.c_str(), port_, this->send_wait_time_);
 }
 
-float ModbusTCP::get_setup_priority() const { return setup_priority::AFTER_WIFI; }
+float ModbusTCP::get_setup_priority() const { return setup_priority::AFTER_WIFI - 1.0f; }
 
 void ModbusTCP::send(uint8_t address, uint8_t function_code, uint16_t start_address, uint16_t number_of_entities, uint8_t payload_len, const uint8_t *payload) {
   static const size_t MAX_VALUES = 128;
@@ -150,7 +186,37 @@ void ModbusTCP::send(uint8_t address, uint8_t function_code, uint16_t start_addr
     ESP_LOGE(TAG, "send too many values %d max=%zu", number_of_entities, MAX_VALUES);
     return;
   }
+  // if (!client.connect(host_.c_str(), port_)) {
+  //    ESP_LOGE("modbus_tcp", "Failed to connect to Modbus server %s:%d", host_.c_str(), port_);
+  //    return;
+  //  }
   
+  size_t MAX_FRAME_SIZE = 256;
+  uint8_t data[MAX_FRAME_SIZE];
+  size_t pos = 0;
+      data[pos++] = Transaction_Identifier >> 8;
+      data[pos++] = Transaction_Identifier >> 0;
+      data[pos++] = 0x00;
+      data[pos++] = 0x00;
+      data[pos++] = 0x00;
+        if (payload != nullptr) { 
+         //  data_send.push_back(0x04 + payload_len);
+        data[pos++] = (0x04 + payload_len);
+        }else {
+        data[pos++] = 0x06;      // how many bytes next comes for request
+      }
+      data[pos++] = address;
+      data[pos++] = function_code;
+      data[pos++] = start_address >> 8;
+      data[pos++] = start_address >> 0;
+
+      if (function_code != 0x05 && function_code != 0x06) {
+      data[pos++] = number_of_entities >> 8;
+      data[pos++] = number_of_entities >> 0;
+      }
+      Transaction_Identifier++;
+ send_message(data);
+  /*
  std::vector<uint8_t> data_send;
  
       data_send.push_back(Transaction_Identifier >> 8);
@@ -173,7 +239,6 @@ void ModbusTCP::send(uint8_t address, uint8_t function_code, uint16_t start_addr
       data_send.push_back(number_of_entities >> 0);
      }
   
-
   if (payload != nullptr) {
     if (function_code == 0x0F || function_code == 0x17) {  // Write multiple
       data_send.push_back(payload_len);                    // Byte count is required for write
@@ -195,13 +260,15 @@ res1 += buf1;
 res1 += ":";
 }
    if (connected_ && client_->canSend()) {
-    client_->write(reinterpret_cast<const char*>(data_send.data()), sizeof(data_send));
+    //if (connected_) {
+    //client_->write(reinterpret_cast<const char*>(data_send.data()), sizeof(data_send));
     Transaction_Identifier++;
+    //client.write(reinterpret_cast<const char*>(data_send.data()), sizeof(data_send));
     ESP_LOGD(TAG, ">>> %02X%02X %02X%02X %02X%02X %02X %02X %02X%02X %02X%02X %s",
                    data_send[0], data_send[1],  data_send[2], data_send[3], data_send[4], data_send[5],
                    data_send[6], data_send[7],  data_send[8], data_send[9], data_send[10], data_send[11], res1.c_str());
    }
-
+*/
 waiting_for_response = address;
 last_send_ = millis();
 }
@@ -212,9 +279,9 @@ void ModbusTCP::send_raw(const std::vector<uint8_t> &payload) {
     return;
   }
 
- if (connected_ && client_->canSend()) {
+ // this->write_array(payload);
   client_->write(reinterpret_cast<const char*>(payload.data()), sizeof(payload));
- }
+  //this->client.clear();
   waiting_for_response = payload[0];
   ESP_LOGV(TAG, "Modbus write raw: %s", format_hex_pretty(payload).c_str());
   last_send_ = millis();
